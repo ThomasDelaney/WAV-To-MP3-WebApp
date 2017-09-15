e89477246824f1589b9d0bdfce44c527d02bef9f
ThomasDelaney <thomasjcdelaney@gmail.com>
2017-09-15 23:14:44 +0100

    error checking and sliding ability added

--- mp3Towav/converter/views.py
+++ mp3Towav/converter/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.core.files.base import ContentFile
 from wsgiref.util import FileWrapper
 from django.shortcuts import HttpResponse
+from django.template import loader
 
 import pydub
 
@@ -13,25 +14,32 @@ def index(request):
 def upload_file(request):
 	if request.method == "POST":
 		#get the wav file that was uploaded by the user
-		wavFile = request.FILES['wavFile']
 
-		#create a space for the mp3 file in the FILES list in the Django server
-		request.FILES['mp3File'] = wavFile
+		if len(request.FILES) != 0:
+			wavFile = request.FILES['wavFile']
 
-		#get the name of the wav file, get rid of the .wav part and append .mp3
-		wavStr = request.FILES['wavFile'].name
-		wavList = wavStr.split(".wav")
-		mp3File = wavList[0] + ".mp3"
+			#create a space for the mp3 file in the FILES list in the Django server
+			request.FILES['mp3File'] = wavFile
 
-		#convert the wav file to mp3 and store it in the mp3 file space we made on the FILES list
-		AudioSegment.from_wav(wavFile).export(request.FILES['mp3File'], format="mp3")
+			#get the name of the wav file, get rid of the .wav part and append .mp3
+			wavStr = request.FILES['wavFile'].name
 
-		#create a httpresponse to return the mp3 file to the user aka the file is downloaded onto the users computer
-		response = HttpResponse(content=request.FILES['mp3File'], content_type='application/mp3')
-		response['Content-Type'] = 'application/mp3'
+			listCheck = wavStr.split(".")
 
-		#make sure the file name will be the previous file name now ending in .mp3
-		response['Content-Disposition'] = 'attachment; filename="%s"'%mp3File
-		return response
+			if listCheck[1] != "wav":
+				return render(request, 'converter/home.html', {'check': 'Please Choose a .WAV File'})
+			else:
+				wavList = wavStr.split(".wav")
+				mp3File = wavList[0] + ".mp3"
+
+				#convert the wav file to mp3 and store it in the mp3 file space we made on the FILES list
+				AudioSegment.from_wav(wavFile).export(request.FILES['mp3File'], format="mp3")
+
+				#create a httpresponse to return the mp3 file to the user aka the file is downloaded onto the users computer
+				response = HttpResponse(content=request.FILES['mp3File'], content_type='application/mp3')
+				response['Content-Disposition'] = 'attachment; filename="%s"'%mp3File
+				return response
+		else:
+			return render(request, 'converter/home.html', {'check': 'Please Choose a .WAV File'})
 	else:
 		return render(request, 'converter/home.html')
